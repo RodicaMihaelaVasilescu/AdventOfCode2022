1824e8593a879a5fb3dbc7b0bf4787d86cd9706c
DESKTOP-J0NQ995\Rodica <vasilescu.rodica@yahoo.com>
2022-12-01 08:09:53 +0200

    --- Day 1: Calorie Counting ---

---
+++ Day 01/Day 1.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cstdio>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <regex>
+#include <sstream>
+#include <numeric>
+
+using namespace std;
+
+int main()
+{
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+
+  string line;
+  vector<int> numbers;
+  int sum = 0, maxSum = 0;
+
+  while (getline(cin, line)) {
+    if (line == "")
+    {
+      maxSum = max(maxSum, sum);
+      numbers.push_back(sum);
+      sum = 0;
+    }
+    else
+    {
+      sum += stoi(line);
+    }
+  }
+  numbers.push_back(sum);
+
+  sort(numbers.rbegin(), numbers.rend());
+
+  // the Elf carrying the most Calories
+  cout << "Part 1: " << numbers[0] << endl;
+  // the top three Elves carrying the most Calories
+  cout << "Part 2: " << accumulate(numbers.begin(), numbers.begin() + 3, 0) << endl;
+}
